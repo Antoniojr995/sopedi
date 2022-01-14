74dc1274c06dd1d865d4e22d743dc85326a2dd61
Antoniojr995 <antoniobsjor@gmail.com>
2022-01-14 16:53:23 -0300

    Melhorando tela de pedido e comanda

--- telas/comanda.tsx
+++ telas/comanda.tsx
@@ -1,3 +1,4 @@
+import { AntDesign, Ionicons } from '@expo/vector-icons';
 import axios from 'axios';
 import React from 'react';
 import { Alert, Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
@@ -5,12 +6,12 @@ import { FlatList, ScrollView } from 'react-native-gesture-handler';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Comanda({route,navigation}:any) {
-  const pedido = route.params.pedido;
   const mesa = route.params.mesa;
   const [comanda,setComanda] = React.useState(route.params.comanda);
   const [comidas,setComidas] = React.useState([]);
   const [loading,setLoading] = React.useState(false);
   const [total,setTotal] = React.useState(0.00);
+  const [atualizar,setAtualizar] = React.useState(null);
   const hasUnsavedChanges = Boolean(false);
   React.useEffect(
     () =>
@@ -25,20 +26,52 @@ export default function Comanda({route,navigation}:any) {
   React.useEffect(() => {
     listComida();
   } ,[mesa,route.params.mesa])
+  function del(index:number){
+    let comanda_temp = comanda;
+    let comidas_temp = comidas;
+    let comida = comidas[index];
+    let busca = {comida:comida.comida.id,qtd:comida.qtd};
+    comanda.map((item,pos)=>{
+      if(item.comida==busca.comida){
+        comanda_temp[pos].qtd -= 1;
+        setTotal(total-comidas[index].comida.preco);
+      }
+    });
+    setAtualizar(busca);
+    comidas_temp[index].qtd -= 1;
+    console.log("DIMINUIR");
+    setComanda(comanda_temp);
+    setComidas(comidas_temp);
+  }
+  function add(index:number){
+    let comanda_temp = comanda;
+    let comidas_temp = comidas;
+    let comida = comidas[index];
+    let busca = {comida:comida.comida.id,qtd:comida.qtd};
+    comanda.map((item,pos)=>{
+      if(item.comida==busca.comida){
+        comanda_temp[pos].qtd += 1;
+        setTotal(total+comidas[index].comida.preco);
+      }
+    });
+    setAtualizar(busca);
+    comidas_temp[index].qtd += 1;
+    console.log("AUMENTAR");
+    setComanda(comanda_temp);
+    setComidas(comidas_temp);
+  }
   async function listComida(){
     let valor_total = 0.00;
     setLoading(false);
     console.log("CARREGANDO");
-    let pedidos = comanda;
-    pedidos.push(...[{id:pedido}]);
     let listagem = [];
     if(comanda){
       comanda.map(async (item)=>{
-        await axios.get(`https://61d5da1d6cb45e001718e069.mockapi.io/comida/${item.id}`)
+        await axios.get(`https://61d5da1d6cb45e001718e069.mockapi.io/comida/${item.comida}`)
           .then(function (response) {
-            listagem.push(...[response.data])
-            valor_total+=response.data.preco;
-            console.log(response.data);
+            let comida_qtd = {comida:response.data,qtd:item.qtd}
+            listagem.push(...[comida_qtd])
+            valor_total+=(response.data.preco*item.qtd);
             if(listagem.length==comanda.length){
               setComidas(listagem);
               setTotal(valor_total);
@@ -54,16 +87,35 @@ export default function Comanda({route,navigation}:any) {
         <View style={{paddingTop:50}}>
           {comidas && loading ? (
             <FlatList data={comidas}
+              keyExtractor={item=>item.comida.id}
+              extraData={atualizar}
+              ListHeaderComponent={(
+                <View style={styles.lista}>
+                  <Text style={styles.lista_text}>COMIDA</Text>
+                  <Text style={styles.lista_text}>QUANTIDADE</Text>
+                  <Text style={styles.lista_text}>PREÇO</Text>
+                </View>
+              )}
               renderItem={({ item,index }) => (
-                <View key={index}>
-                  <Text>PEDIDO - {item.nome}</Text>
+                <View style={styles.lista} key={index}>
+                  <Text style={styles.lista_text}>{item.comida.nome}</Text>
+                  <View style={styles.lista_view}>
+                    <TouchableOpacity onPress={()=>{del(index)}} style={styles.lista_btn}>
+                      <Ionicons name="remove-circle-outline" size={24} color="black" />
+                    </TouchableOpacity>
+                    <Text style={styles.lista_text}>{item.qtd}</Text>
+                    <TouchableOpacity onPress={()=>{add(index)}} style={styles.lista_btn}>
+                      <Ionicons name="add-circle-outline" size={24} color="black" />
+                    </TouchableOpacity>
+                  </View>
+                  <Text style={styles.lista_text}>R$ {item.qtd*item.comida.preco}</Text>
                 </View>
               )}
             />
           ):(
             <View><Text>CARREGANDO</Text></View>
           )}
-          <Text>Total R$ {total.toString()}</Text>
+          <Text style={styles.lista_text}>Total R$ {total.toString()}</Text>
           <Button
             onPress={()=>{
               navigation.navigate("Menu",{mesa:mesa,comanda:comanda})
@@ -82,13 +134,6 @@ export default function Comanda({route,navigation}:any) {
             title="Fazer Pedido"
             color="#0000FF"
           />
-           <Button
-            onPress={()=>{
-              navigation.navigate("PedidoFinalizado")
-            }}
-            title="Finalizar Pedido"
-            color="#0000FF"
-          />
         </View>
     </SafeAreaView>
   );
@@ -101,4 +146,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  lista:{
+    flexDirection: 'row',
+    borderBottomWidth: 2
+  },
+  lista_view:{
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignContent: 'center',
+    textAlign:'center',
+    width: '33%'
+  },
+  lista_btn:{
+    alignContent:'center',
+    alignItems:'center',
+    textAlign:'center',
+    width: '33%'
+  },
+  lista_text:{
+    fontSize:20,
+    textAlign:'center',
+    width: '33%'
+  }
 });
--- telas/pedido.tsx
+++ telas/pedido.tsx
@@ -1,24 +1,26 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { FlatList, ScrollView } from 'react-native-gesture-handler';
+import { StyleSheet, Text, TouchableOpacity, View, SectionList } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { BouncyCheckbox } from "react-native-bouncy-checkbox";
-import { DrawerItem } from '@react-navigation/drawer';
+import Checkbox from 'expo-checkbox';
 const axios = require('axios');
-
 
 export default function Pedido({route,navigation}:any) {
   const tipo = route.params.tipo;
   const mesa = route.params.mesa;
-  const comanda = route.params.comanda;
-  const [comidas, setComidas] = useState([]);
+  const [comanda,setComanda] = useState(route.params.comanda);
+  const [comidas,setComidas] = useState([]);
+  const [atualizar,setAtualizar] = useState(null);
+  const [pedido,setPedido] = useState([]);
   useEffect(() => {
     let tipos = [];
     axios.get("https://61d5da1d6cb45e001718e069.mockapi.io/comida")
       .then(function (response:any) {
         response.data.map((item,index)=>{
-          console.log(item);
           item.checked = false;
+          item.setChecked = (valor:boolean)=>{
+            item.checked=valor;
+          }
           console.log(item);
           if(item.tipo==tipo){
             tipos.push(item);
@@ -26,26 +28,68 @@ export default function Pedido({route,navigation}:any) {
         })
         setComidas(tipos);
       })
-  } ,[])
-  return (
-    <SafeAreaView style={styles.container}>
-      <FlatList data={comidas} renderItem={(item)=>(
-        <View>
-          {item.item.checked ? (
-            <TouchableOpacity onPress={()=>{console.log("MUDANDO");item.item.checked=false}}>
-              <Text>OK</Text>
-            </TouchableOpacity>
-            ):(
-              <TouchableOpacity onPress={()=>{console.log("MUDANDO");item.item.checked=true}}>
-                <Text>ADICIONAR</Text>
-              </TouchableOpacity>
-            )}
-        <TouchableOpacity onPress={()=>{navigation.navigate('comanda',{pedido:item.item.id,mesa:mesa,comanda:comanda})}}>
+  } ,[]);
+  function renderItem(item){
+    return(
+      <View>
+          <Checkbox
+            value={item.item.checked}
+            onValueChange={(val)=>{
+              item.item.setChecked(!item.item.checked);
+              if(item.item.checked){
+                setAtualizar(item.item);
+                let pedido_temp = pedido;
+                let inserir = {comida:item.item.id,qtd:1};
+                pedido_temp.push(...[inserir]);
+                setPedido(pedido_temp);
+              }else{
+                setAtualizar(null);
+                let pedido_temp = pedido;
+                item.item.setChecked(!item.item.checked);
+                let busca = {comida:item.item.id,qtd:1};
+                let remover = pedido_temp.indexOf(busca);
+                if (remover > -1) {
+                  pedido_temp.splice(remover, 1);
+                }
+                item.item.setChecked(!item.item.checked);
+                setPedido(pedido_temp);
+              }
+            }}
+          />
           <Text>{item.item.nome}</Text>
           <Text>R$ {item.item.preco.toString()}</Text>
-        </TouchableOpacity>
       </View>
-      )} keyExtractor={item => item.id}/>
+    );
+  }
+  function enviarComanda(){
+    let comanda_temp = comanda;
+    let pedido_temp = pedido;
+    comanda.map((item,index)=>{
+      pedido.map((item2)=>{
+        if(item.comida==item2.comida){
+          comanda_temp[index].qtd+=1;
+          let remover = pedido_temp.indexOf(item2);
+          if (remover > -1) {
+            pedido_temp.splice(remover, 1);
+          }
+        }
+      })
+    })
+    comanda_temp.push(...pedido_temp);
+    setComanda(comanda_temp);
+    navigation.navigate('comanda',{mesa:mesa,comanda:comanda})
+  }
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={comidas}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+        extraData={atualizar}
+      />
+      <TouchableOpacity onPress={()=>{enviarComanda()}}>
+        <Text>Adicionar na comanda</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
